b2dc0a93f192639d6afa0b4affc38b4d61f69d50
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-11 16:39:04 -0300

    alterou os comandos fundamentais do javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,66 @@
 console.log("Teste")
+
+console.log("Comando de saída")
+
+//COMENTÁRIO DE LINHA SIMPLES
+
+/*
+COMENTARIO DE MÚLTIPLAS LINHAS, OU SEJA, POSSO DESCREVER ALGO NESSE TIPO DE COMENTÁRIO...
+
+*/
+
+//CRIANDO VARIÁVEIS
+let num = 50
+var num2 = 180
+const num3 = 450
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+if (true){
+    let num4 = 420
+ console.log(num4)   
+}
+
+console.log(num5)
+
+//num3 = 12
+
+//Operadores matemáticos
+/*
+
+ + SOMA
+ - SUBTRAÇÃO 
+ * MULTIPLICAÇÃO
+ % MÓDULO (RESTO DA DIVISÃO)
+*/
+
+console.log('SOMA DAS VARIÁVEIS', num + num2)
+console.log("SUBTRAÇÃO DAS VARIÁVEL", num3 - num)
+console.log("MULTIPLICAÇÃO", num5 * 2)
+console.log("DIVISÃO DE VALORES", num2 / 5)
+console.log("RESTO DA DIVISÃO", num2 % 2)
+console.log("RESTO DA DIVISÃO ${num3 % 3}")
+
+//OPERADORES RELACIONADOS
+/*
+
+ == IGUAL OU COMPARAÇÃO
+ = Atribuição
+ > MAIOR
+ < MENOR
+ <= MENOR IGUAL
+ != DIFERENTE
+*/
+
+//OPERADORES LÓGICO
+/*
+   && E
+   || OU
+   */
+  console.log ("TESTE DE RELACIONAIS", 10 == 5)
+  console.log("TESTE RELACIONAIS", 10 != 5)
+  console.log("TESTE RELACIONAIS", 10 > 5)
+  console.log("TESTE RELACIONAIS", 10 < 5)
+  console.log("TESTE RELACIONAIS")
